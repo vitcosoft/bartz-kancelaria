71d083aa0cc7bc111ef4d22148fb64ad58d600d6
witek <witek.wozniak@pm.me>
2025-05-17 16:02:47 +0200

    initial commmit v2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/ui']
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+    build: {
+      sourcemap: true,
+    },
+    css: {
+      devSourcemap: true,
+    },
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui'
+  ]
 })
