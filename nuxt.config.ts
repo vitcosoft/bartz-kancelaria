e5a54f8ba03a0b195ed1930d1a8890a2c51b2e5c
witek <witek.wozniak@pm.me>
2025-06-03 18:54:45 +0200

    Add NuxtUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
   vite: {
     plugins: [
       tailwindcss(),
