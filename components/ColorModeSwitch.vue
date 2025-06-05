ae1d681329ab495c692ca2739bbf7c069cf3629b
witek <witek.wozniak@pm.me>
2025-06-05 21:17:59 +0200

    Implement color mode switch

---
+++ components/ColorModeSwitch.vue
@@ -0,0 +1,23 @@
+<template>
+  <ClientOnly v-if="!colorMode?.forced">
+    <USwitch
+      v-model="isDark"
+      unchecked-icon="material-symbols:light-mode-outline"
+      checked-icon="material-symbols:moon-stars-outline"
+      size="lg"
+    />
+  </ClientOnly>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set(_isDark) {
+    colorMode.preference = _isDark ? 'dark' : 'light'
+  },
+})
+</script>
